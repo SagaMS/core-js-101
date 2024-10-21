db58c99a5fc2bc0b3f91e60587619a972302a32a
SagaMS <91miablond@gmail.com>
2024-10-21 14:30:28 +0700

    feat:solve task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -35,10 +35,11 @@ function findElement(arr, value) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  let arr = Array.from(Array(len * 2));
+  arr = arr.map((elem, index) => index + 1);
+  return arr.filter((n) => n % 2 !== 0);
 }
-
 
 /**
  * Returns the doubled array - elements of the specified array
