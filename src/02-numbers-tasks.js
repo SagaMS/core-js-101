cfd2a92a9c8fc0e8a87c236ac5359513ee52bb30
SagaMS <91miablond@gmail.com>
2024-10-21 10:11:21 +0700

    feat:solve task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -228,8 +228,12 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const a = Number(value);
+  if (Number.isNaN(a)) {
+    return def;
+  }
+  return a;
 }
 
 module.exports = {
