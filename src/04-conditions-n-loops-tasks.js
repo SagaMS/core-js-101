c3379346f6312b4b59f988c1be852e076d566124
SagaMS <91miablond@gmail.com>
2024-10-28 03:29:30 +0700

    feat:solve task

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -297,8 +297,12 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let number = num;
+  while (number > 10) {
+    number = String(number).split('').map((item) => Number(item)).reduce((sum, i) => sum + i, 0);
+  }
+  return number;
 }
 
 
