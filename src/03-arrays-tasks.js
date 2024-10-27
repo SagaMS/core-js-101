1688760ae52fe6b5e4d089407ac4f91b2e41b093
SagaMS <91miablond@gmail.com>
2024-10-27 18:58:33 +0700

    feat:solve task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -386,8 +386,9 @@ function getItemsSum(arr) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  const newArray = arr.filter((i) => Boolean(i) === false);
+  return newArray.length;
 }
 
 /**
